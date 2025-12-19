3814bcc234a59c4f912bd805e8f5d3d88916ca2e
sleepy-mango280 <kingarvin3rd@gmail.com>
2025-12-18 21:02:48 -0600

    Implement flywheel tuning functionality

---
+++ flywheelTelemetry.java
@@ -0,0 +1,86 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.PIDFCoefficients;
+
+@TeleOp
+public class flywheelTuner extends OpMode {
+
+    public DcMotorEx flywheelMotor;
+
+    public double highVelocity = 1500;
+    public double lowVelocity = 900;
+
+    double curTargetVelocity = highVelocity;
+
+    double F = 0;
+
+    double P = 0;
+
+    double[] stepSizes = {10.0, 1.0, 0.1, 0.001, 0.0001};
+
+    int stepIndex = 1;
+
+    @Override
+    public void init() {
+        flywheelMotor = hardwareMap.get(DcMotorEx.class, "motor");
+        flywheelMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        flywheelMotor.setDirection(DcMotorSimple.Direction.REVERSE);
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+        flywheelMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+        telemetry.addLine("Init complete");
+
+
+
+    }
+
+    @Override
+    public void loop() {
+        if(gamepad1.yWasPressed()) {
+            if (curTargetVelocity == highVelocity) {
+                curTargetVelocity = lowVelocity;
+            } else {curTargetVelocity = highVelocity;}
+        }
+
+        if (gamepad1.bWasPressed()) {
+            stepIndex = (stepIndex + 1) % stepSizes.length;
+        }
+
+        if (gamepad1.dpadLeftWasPressed()) {
+            F -= stepSizes[stepIndex];
+        }
+
+        if (gamepad1.dpadRightWasPressed()) {
+            F += stepSizes[stepIndex];
+        }
+
+        if (gamepad1.dpadDownWasPressed()) {
+            P -= stepSizes[stepIndex];
+        }
+
+        if (gamepad1.dpadUpWasPressed()) {
+            P += stepSizes[stepIndex];
+        }
+
+        PIDFCoefficients pidfCoefficients = new PIDFCoefficients(P, 0, 0, F);
+        flywheelMotor.setPIDFCoefficients(DcMotor.RunMode.RUN_USING_ENCODER, pidfCoefficients);
+
+        flywheelMotor.setVelocity(curTargetVelocity);
+
+        double curVelocity = flywheelMotor.getVelocity();
+        double error = curTargetVelocity - curVelocity;
+
+        telemetry.addData("target Velocity", curTargetVelocity);
+        telemetry.addData("current velocity", "%.2f", curVelocity);
+        telemetry.addData("Error", "%.2f", error);
+        telemetry.addLine("----------------------");
+        telemetry.addData("Tuning P", "%.4f (D-Pad U/D)", P);
+        telemetry.addData("Tuning F", "%.4f (D-Pad L/R", F);
+        telemetry.addData("Step Size", "%.4f (B Button)", stepSizes[stepIndex]);
+
+    }
+}
