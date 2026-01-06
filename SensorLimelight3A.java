eca58db699b254428e296be7cebee80ba94595f6
sleepy_mango <kingarvin3rd@gmail.com>
2026-01-05 21:01:43 -0600

    Refactor SensorLimelight3A class and update comments

--- SensorLimelight3A.java
+++ SensorLimelight3A.java
@@ -1,12 +1,73 @@
+/*
+Copyright (c) 2024 Limelight Vision
+
+All rights reserved.
+
+Redistribution and use in source and binary forms, with or without modification,
+are permitted (subject to the limitations in the disclaimer below) provided that
+the following conditions are met:
+
+Redistributions of source code must retain the above copyright notice, this list
+of conditions and the following disclaimer.
+
+Redistributions in binary form must reproduce the above copyright notice, this
+list of conditions and the following disclaimer in the documentation and/or
+other materials provided with the distribution.
+
+Neither the name of FIRST nor the names of its contributors may be used to
+endorse or promote products derived from this software without specific prior
+written permission.
+
+NO EXPRESS OR IMPLIED LICENSES TO ANY PARTY'S PATENT RIGHTS ARE GRANTED BY THIS
+LICENSE. THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+"AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO,
+THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR
+TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF
+THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+*/
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.hardware.limelightvision.LLResult;
+import com.qualcomm.hardware.limelightvision.LLResultTypes;
+import com.qualcomm.hardware.limelightvision.LLStatus;
 import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
-@TeleOp(name = "Sensor: Limelight3A Driver Assist", group = "Sensor")
-public class SensorLimelight3ADriverAssist extends LinearOpMode {
+import org.firstinspires.ftc.robotcore.external.navigation.Pose3D;
+
+import java.util.List;
+
+/*
+ * This OpMode illustrates how to use the Limelight3A Vision Sensor.
+ *
+ * @see <a href="https://limelightvision.io/">Limelight</a>
+ *
+ * Notes on configuration:
+ *
+ *   The device presents itself, when plugged into a USB port on a Control Hub as an ethernet
+ *   interface.  A DHCP server running on the Limelight automatically assigns the Control Hub an
+ *   ip address for the new ethernet interface.
+ *
+ *   Since the Limelight is plugged into a USB port, it will be listed on the top level configuration
+ *   activity along with the Control Hub Portal and other USB devices such as webcams.  Typically
+ *   serial numbers are displayed below the device's names.  In the case of the Limelight device, the
+ *   Control Hub's assigned ip address for that ethernet interface is used as the "serial number".
+ *
+ *   Tapping the Limelight's name, transitions to a new screen where the user can rename the Limelight
+ *   and specify the Limelight's ip address.  Users should take care not to confuse the ip address of
+ *   the Limelight itself, which can be configured through the Limelight settings page via a web browser,
+ *   and the ip address the Limelight device assigned the Control Hub and which is displayed in small text
+ *   below the name of the Limelight on the top level configuration screen.
+ */
+@TeleOp(name = "Sensor: Limelight3A", group = "Sensor")
+public class SensorLimelight3A extends LinearOpMode {
 
     private Limelight3A limelight;
 
@@ -17,19 +78,31 @@ public class SensorLimelight3ADriverAssist extends LinearOpMode {
         telemetry.setMsTransmissionInterval(11);
 
         limelight.pipelineSwitch(0);
+
+        /*
+         * Starts polling for data.  If you neglect to call start(), getLatestResult() will return null.
+         */
         limelight.start();
 
-        telemetry.addData(">", "Robot Ready. Press Play.");
+        telemetry.addData(">", "Robot Ready.  Press Play.");
         telemetry.update();
         waitForStart();
 
         while (opModeIsActive()) {
+            LLStatus status = limelight.getStatus();
+            telemetry.addData("Name", "%s",
+                    status.getName());
+            telemetry.addData("LL", "Temp: %.1fC, CPU: %.1f%%, FPS: %d",
+                    status.getTemp(), status.getCpu(), (int) status.getFps());
+            telemetry.addData("Pipeline", "Index: %d, Type: %s",
+                    status.getPipelineIndex(), status.getPipelineType());
 
             LLResult result = limelight.getLatestResult();
 
+            //  ALIGNMENT & DISTANCE INDICATOR 
             boolean shoot = false;
 
-            //  INPUT VALUES HERE 
+//  INPUT VALUES HERE 
             double TX_THRESHOLD = input ...;   // degrees left/right tolerance (e.g., 1.0)
             double TY_MIN = input ...;         // min vertical angle for “close enough”
             double TY_MAX = input ...;         // max vertical angle for “close enough”
@@ -39,10 +112,12 @@ public class SensorLimelight3ADriverAssist extends LinearOpMode {
                 double tx = result.getTx();  // horizontal offset
                 double ty = result.getTy();  // vertical offset (proxy for distance)
 
-                // ===== DRIVER DIRECTION ASSIST (MESSAGE ONLY) =====
+
+                // ===== DRIVER DIRECTION ASSIST (SINGLE MESSAGE) =====
+
                 String assistMessage;
 
-                // Priority: left/right first, then distance
+// Priority: left/right first, then distance
                 if (tx > TX_THRESHOLD) {
                     assistMessage = "TURN RIGHT";
                 } else if (tx < -TX_THRESHOLD) {
@@ -57,22 +132,25 @@ public class SensorLimelight3ADriverAssist extends LinearOpMode {
 
                 telemetry.addData("Driver Assist", assistMessage);
 
-                //  SHOOT DECISION 
+
+//  TELEMETRY 
+
                 boolean aligned = Math.abs(tx) < TX_THRESHOLD;
                 boolean inRange = (ty > TY_MIN && ty < TY_MAX);
-                shoot = aligned && inRange;
 
-                // Optional debug
+                if (aligned && inRange) {
+                    shoot = true;
+                }
+
                 telemetry.addData("tx", tx);
                 telemetry.addData("ty", ty);
                 telemetry.addData("Aligned?", aligned);
                 telemetry.addData("In Range?", inRange);
 
             } else {
-                telemetry.addData("Driver Assist", "NO TARGET");
+                telemetry.addLine("NO TARGET");
             }
 
-            //  SHOOT / DON'T SHOOT 
             if (shoot) {
                 telemetry.addLine("====== SHOOT ======");
             } else {
@@ -81,7 +159,6 @@ public class SensorLimelight3ADriverAssist extends LinearOpMode {
 
             telemetry.update();
         }
-
         limelight.stop();
     }
 }
